c365f27c9b765a7f1d9eb4be51a83b817c552666
TheBlubb14 <TheBlubb14@users.noreply.github.com>
2021-12-28 21:09:15 +0100

    Manual update check and quick link
    When opening the about page we will check for a newer version of the app on the github releases with a higher tag.
    If found the button will indicate, that there is a new version available and the button will be clickable to directly download the new .apk version.

--- lib/screens/about_page.dart
+++ lib/screens/about_page.dart
@@ -3,8 +3,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:smarthome/helper/theme_manager.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:github/github.dart';
+import 'package:version/version.dart';
 
 class AboutScreen extends StatelessWidget {
+  static final Version version = new Version(1, 1, 5);
+  final GitHub gitHub = new GitHub();
+  final RepositorySlug repositorySlug =
+      new RepositorySlug("susch19", "SmartHome");
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,8 +22,50 @@ class AboutScreen extends StatelessWidget {
     );
   }
 
+  String getVersionString(Version? newVersion) {
+    var v = version.toString();
+
+    if (newVersion != null) {
+      v += " (neue Version " + newVersion.toString() + " verfügbar)";
+    }
+
+    return v;
+  }
+
+  Future<VersionAndUrl?> getVersionAndUrl() async {
+    var v = await getNewVersion();
+    var url = await newVersionUrl();
+
+    return new VersionAndUrl(v, url);
+  }
+
+  Future<Version?> getNewVersion() {
+    return gitHub.repositories.listTags(repositorySlug).asyncMap((element) {
+      try {
+        return Version.parse(element.name.replaceFirst(RegExp(r'v|V'), ''));
+      } catch (e) {
+        // TODO: log
+        return null;
+      }
+    }).firstWhere((element) => element != null && element > version);
+  }
+
+  Future<String?> newVersionUrl() async {
+    var release = await gitHub.repositories.getLatestRelease(repositorySlug);
+    return release.assets
+        ?.firstWhere((element) =>
+            element.contentType == "application/vnd.android.package-archive")
+        .browserDownloadUrl;
+  }
+
+  void openUrl(String url) async {
+    await launch(url, forceWebView: true);
+  }
+
   Widget buildBody(BuildContext context) {
-    var iconColor = AdaptiveTheme.of(context).mode == AdaptiveThemeMode.dark ? Colors.white : Colors.black;
+    var iconColor = AdaptiveTheme.of(context).mode == AdaptiveThemeMode.dark
+        ? Colors.white
+        : Colors.black;
     return Container(
       decoration: ThemeManager.getBackgroundDecoration(context),
       child: ListView(
@@ -55,7 +104,7 @@ class AboutScreen extends StatelessWidget {
           ),
           ListTile(
             title: Text(
-              "Tolle beschreibung dieser Smarthome App",
+              "Tolle Beschreibung dieser Smarthome App",
               textAlign: TextAlign.center,
             ),
             // "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."),
@@ -72,9 +121,25 @@ class AboutScreen extends StatelessWidget {
               "Entwickelt von susch19 (Sascha Hering)",
             ),
           ),
-          ListTile(
-            title: Text("Version 0.1"),
-          ),
+          FutureBuilder<VersionAndUrl?>(
+              future: getVersionAndUrl(),
+              builder: (context, AsyncSnapshot<VersionAndUrl?> snapshot) {
+                return ListTile(
+                    title: Row(children: [
+                      Text(getVersionString(snapshot.data?.version)),
+                      if (!snapshot.hasData && !snapshot.hasError)
+                        Padding(
+                          padding: const EdgeInsets.only(left: 8),
+                          child: CircularProgressIndicator(),
+                        )
+                    ]),
+                    onTap: () {
+                      if (snapshot.data?.url != null) {
+                        openUrl(snapshot.data!.url!);
+                      }
+                    });
+              }),
+
           Divider(),
           ListTile(
             leading: SvgPicture.asset(
@@ -83,34 +148,22 @@ class AboutScreen extends StatelessWidget {
               width: 32,
             ),
             title: Text("Schau doch mal in den Code auf GitHub rein"),
-            onTap: () {
-              var urlString = "https://github.com/susch19/smarthome";
-              canLaunch(urlString).then((value) {
-                if (value) launch(urlString);
-              });
-            },
+            onTap: () => openUrl("https://github.com/susch19/smarthome"),
           ),
           Divider(),
           ListTile(
-            leading: SvgPicture.asset("assets/vectors/smarthome_icon.svg", alignment: Alignment.center, width: 32),
-            title: Text("Wer hat dieses schicke Icon gemacht? Finde es heraus!"),
-            onTap: () {
-              var urlString = "https://iconarchive.com/show/flatwoken-icons-by-alecive/Apps-Home-icon.html";
-              canLaunch(urlString).then((value) {
-                if (value) launch(urlString);
-              });
-            },
+            leading: SvgPicture.asset("assets/vectors/smarthome_icon.svg",
+                alignment: Alignment.center, width: 32),
+            title:
+                Text("Wer hat dieses schicke Icon gemacht? Finde es heraus!"),
+            onTap: () => openUrl(
+                "https://iconarchive.com/show/flatwoken-icons-by-alecive/Apps-Home-icon.html"),
           ),
           // Divider(),
           // ListTile(
           //   leading: SvgPicture.asset("assets/vectors/google_play.svg", alignment: Alignment.center, width: 32),
           //   title: Text("Play Store Eintrag"),
-          //   onTap: () {
-          //     var urlString = "https://play.google.com/store/apps/details?id=de.susch19.nssl";
-          //     canLaunch(urlString).then((value) {
-          //       if (value) launch(urlString);
-          //     });
-          //   },
+          //   onTap: () => openUrl("https://play.google.com/store/apps/details?id=de.susch19.nssl"),
           // ),
         ],
       ),
